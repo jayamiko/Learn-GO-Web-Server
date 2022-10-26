eb179e9712300ec5c13c4af43bd236a1b91508a5
jayamiko <jayamiko4@gmail.com>
2022-10-26 09:53:52 +0700

    Display text to html template

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -12,9 +13,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Hello!")
+		var data = map[string]string{
+			"Name": "Jaya Miko",
+			"Message": "Have a Nice Day!",
+		}
+
+		var t, err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
 	})
-	http.HandleFunc("/index", index)
+	// http.HandleFunc("/index", index)
 
 	fmt.Println("Starting web Server at http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
