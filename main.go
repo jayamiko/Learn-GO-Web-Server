2bfd765e003309f7fd39755851efe16519212c52
jayamiko <jayamiko4@gmail.com>
2022-10-26 16:42:31 +0700

    HTTP Request fetch API

--- main.go
+++ main.go
@@ -3,77 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
-// Web Service API adalah sebuah web yang menerima request dari client dan menghasilkan response, biasa berupa JSON/XML.
-
-type student struct {
-	ID    string
-	Name  string
-	Age   int
-	Title string
-}
-
-// Json Data
-var data = []student{
-	student{"1", "Jaya Miko", 24, "Programmer"},
-	student{"2", "Leanne Graham", 24, "Designer"},
-	student{"3", "John Wick", 24, "Supervisor"},
-	student{"4", "Ethan Bond", 24, "HRD"},
+// A struct to map our Pokemon's Species which includes it's name
+type Users struct {
+	ID       string `json:"id"`
+	Name     string
+	Username string
+	Email    string
 }
 
 func main() {
-	http.HandleFunc("/users", users)
-	http.HandleFunc("/user", user)
-
-	fmt.Println("Starting web server at http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
-}
-
-// Mengembalikan semua data yang ada di array
-func users(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method == "GET" {
-		var result, err = json.Marshal(data)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(result)
-		return
+	response, err := http.Get("https://jsonplaceholder.typicode.com/users")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
-	http.Error(w, "", http.StatusBadRequest)
-}
-
-// Mengembalikan satu data, diambil dari data berdasarkan ID-nya
-func user(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if r.Method == "GET" {
-		id := r.FormValue("id")
-		var result []byte
-		var err error
-
-		for _, each := range data {
-			if each.ID == id {
-				result, err = json.Marshal(each)
-
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-
-				w.Write(result)
-				return
-			}
-		}
-		http.Error(w, "User Not Found", http.StatusNotFound)
-		return
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
-	http.Error(w, "", http.StatusBadRequest)
+
+	var responseObject []Users
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject)
+
+	for i := 0; i < len(responseObject); i++ {
+		fmt.Println(i, responseObject[i].Email)
+	}
 }
