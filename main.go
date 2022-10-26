62ab8ade93a386e50b471f5ef13dd2e64975599c
jayamiko <jayamiko4@gmail.com>
2022-10-26 13:44:28 +0700

    Json Data: String Json > Object, ArrayJson > Array Object, Object > StringJson

--- main.go
+++ main.go
@@ -1,33 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
-	"text/template"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "How are You?")
+type User struct {
+	FullName string `json:"Name"`
+	Age      int
+	Title    string
 }
 
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		var data = map[string]string{
-			"Name": "Jaya Miko",
-			"Message": "Have a Nice Day!",
-		}
+	stringJsonToObject()
+	arrayJsonToArrayObject()
+	objectToStringJson()
+}
 
-		var t, err = template.ParseFiles("template.html")
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+func stringJsonToObject() {
+	// String JSON to Object
 
-		t.Execute(w, data)
-	})
-	// http.HandleFunc("/index", index)
+	jsonString := `{"Name": "Jaya Miko", "Age": 24, "Title": "Programmer"}`
+	jsonData := []byte(jsonString)
 
-	fmt.Println("Starting web Server at http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
+	var data User
+
+	// json.Unmarshal = json string dikonversi menjadi bentuk objek
+	err := json.Unmarshal(jsonData, &data)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Full Name: ", data.FullName)
+	fmt.Println("Age: ", data.Age)
+	fmt.Println("Title: ", data.Title)
+}
+
+func arrayJsonToArrayObject() {
+	// Array JSON to Array Object
+
+	var jsonStringArray = `[
+		{"Name": "Jaya Miko", "Age": 24, "Title": "Programmer"},
+		{"Name": "Leanne Graham", "Age": 32}
+	]`
+
+	var dataArray []User
+
+	e := json.Unmarshal([]byte(jsonStringArray), &dataArray)
+
+	if e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+
+	fmt.Println("FullName: ", dataArray[0].FullName) // Jaya Miko
+	fmt.Println("Age: ", dataArray[0].Age)           // 24
+	fmt.Println("Title: ", dataArray[0].Title)       // Programmer
+}
+
+func objectToStringJson() {
+	// Object to JSON String
+
+	object := []User{{"Jaya Miko", 24, "Programmer"}, {"Leanne Graham", 27, "Designer"}}
+
+	// json.Marshal = encoding daa ke json string
+	jsonDataObject, err := json.Marshal(object)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	jsonStringObject := string(jsonDataObject)
+	fmt.Println(jsonStringObject)
 }
